f88309ad6fb00cb3e5bf6435a0f103f582a83607
MamyDinyah <mamydinyah34@gmail.com>
2025-03-09 19:48:30 +0300

    express ok

--- express/app.js
+++ express/app.js
@@ -1,11 +1,117 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require("express");
+require("dotenv").config();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server listening on ${port}`);
+});
+
+const generateToken = (user) => {
+  return jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
+};
+
+app.post('/register', async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await prisma.user.create({
+      data: {name, email, password: hashedPassword}
+    });
+    const token = generateToken(user);
+    res.status(201).json({ user, token });
+  } catch (error) {
+    res.status(500).json({ error: 'Erreur lors de l\'inscription' });
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user || !await bcrypt.compare(password, user.password)) {
+      return res.status(401).json({ error: 'Email ou mot de passe incorrect' });
+    }
+    const token = generateToken(user);
+    res.json({ user, token });
+  } catch (error) {
+    res.status(500).json({ error: 'Erreur lors de la connexion' });
+  }
+});
+
+const authenticateJWT = (req, res, next) => {
+  const token = req.header('Authorization')?.split(' ')[1];
+  //console.log('Toke:', token);
+  if (!token) return res.status(403).json({ error: 'Token manquant' });
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      console.error('Erreur JWT:', err);
+      return res.status(403).json({ error: 'Token invalide' });
+    }
+    req.user = user;
+    next();
+  });
+};
+
+app.get('/users', async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Erreur lors de la récupération des utilisateurs' });
+  }
+});
+
+app.get('/users/:id', authenticateJWT, async (req, res) => {
+  try {
+    const userId = parseInt(req.params.id);
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      return res.status(404).json({ error: 'Utilisateur non trouvé' });
+    }
+
+    res.json(user);
+  } catch (error) {
+    console.error('Erreur lors de la récupération de l\'utilisateur:', error);
+    res.status(500).json({ error: 'Erreur lors de la récupération de l\'utilisateur' });
+  }
+});
+
+app.put('/users/:id', authenticateJWT, async (req, res) => {
+  try {
+    const userId = parseInt(req.params.id);
+    const { name, email, password } = req.body;
+    const updatedUser = await prisma.user.update({
+      where: { id: userId },
+      data: { name, email, password },
+    });
+    res.json(updatedUser);
+  } catch (error) {
+    console.error('Erreur lors de la mise à jour de l\'utilisateur:', error);
+    res.status(500).json({ error: 'Erreur lors de la mise à jour de l\'utilisateur' });
+  }
+});
+
+app.delete('/users/:id', authenticateJWT, async (req, res) => {
+  try {
+    const userId = parseInt(req.params.id);
+    const deletedUser = await prisma.user.delete({
+      where: { id: userId },
+    });
+    res.json(deletedUser);
+  } catch (error) {
+    console.error('Erreur lors de la suppression de l\'utilisateur:', error);
+    res.status(500).json({ error: 'Erreur lors de la suppression de l\'utilisateur' });
+  }
 });
